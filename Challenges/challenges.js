7782db265498db94ceea3205eabc5e6488a88ba9
auron1223 <marcuslyons@u.boisestate.edu>
2015-09-09 19:22:22 -0600

    Added the challenges.js file

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -30,5 +30,42 @@ function countLetr(a){
     
 };
 
+// Find the first non-repeating character in a string and return it
+
+// First attempt, iterates over every character so not super efficient
+
+var test = "ABCCA";
 
 
+function findTheFirst(string){
+    var char = {};
+    for(var i = 0; i < string.length; i++){
+        if(char[string[i]]){
+            char[string[i]] = '0';
+        } else {
+            char[string[i]] = '1';
+        }
+    }    
+    for(var j = 0; j < string.length; j++){
+        if(char[string[j]] === '1'){
+          return string.charAt(j);      
+        }
+    }
+
+    return 'None to return';
+}
+
+// Refactor of the above to search front and back as well as not look over duplicates
+
+// Need to do this later using .indexOf() and .lastIndexOf()
+
+// create the forEach method on the String object
+
+String.prototype.forEach = function(callback){
+
+    for(var i = 0; i < this.length; i++){
+        callback(this[i], i, this);
+            
+    }
+
+};
